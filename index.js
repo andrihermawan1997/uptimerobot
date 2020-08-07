f8b43839391dd088110c6b1a9cc81a81ba0ee3de
Andri Hermawan <52045774+andrihermawan1997@users.noreply.github.com>
2020-08-07 18:32:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://xeon2.glitch.me"]
+const urls = ["https://zero-xeon1.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
