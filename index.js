8ad63044153738045f2d20873f4153e8fdaa6649
Andri Hermawan <52045774+andrihermawan1997@users.noreply.github.com>
2020-08-21 12:56:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://music-zeroxeon01.glitch.me"]
+const urls = ["https://https://zero-xeon01.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
